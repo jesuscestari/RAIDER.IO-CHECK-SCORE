9eda52ad5300a933b0244086ab149ac95165ad99
Lolo <jescesta@gmail.com>
2022-11-14 15:01:06 -0300

    fix event

--- raider/src/App.jsx
+++ raider/src/App.jsx
@@ -5,19 +5,42 @@ import './App.css'
 import { useEffect } from 'react'
 
 
+
 function App() {
+
+  const URL = "https://raider.io/api/v1/characters/profile?region=us&realm=area-52&name=NerfTank&fields=mythic_plus_scores";
+
+ const getScore = async () => {
+ const response = await fetch(URL);
+
+ const body = await response.json();
+
+ return body.mythic_plus_scores.all;
+
+};
+
   const [count, setCount] = useState(0)
 
-  const [wAffix, setWAffix] = useState("");
+  const [region, setRegion] = useState("");
+
+  const [realm, setRealm] = useState("");
+
+  const [name, setName] = useState("");
+
+  const [score, setScore] = useState("");
+
+  const [url ,setUrl] = useState(null);
+
+  const onClickHandler = async () => {
+    const url = await getScore();
+
+    setUrl(url);
+  };
 
 
-  const fetchwAffix = () => {
-    Axios.get(`https://raider.io/api/v1/mythic-plus/affixes?region=us&locale=en`).then((res) => {
-    setWAffix(res.data.title);
-  });
-}
 
-  
+
+
   return (
     <div className="App">
       <div>
@@ -27,34 +50,33 @@ function App() {
       </div>
       <h1>Check Raider.IO</h1>
 
-      <form>
-      <div className="card">
-
-  <select name="region"  className="buttonn">
+  <div className="card">
+  
+  <select name="region" className="buttonn" onChange={event => setRegion(event.target.value)}>
   <option value="us">US</option>
   <option value="eu">EU</option>
   <option value="tw">TW</option>
   <option value="kr">KR</option>
   <option value="cn">CN</option>
-</select>
+  
+</select> 
 
 <label>
-<input className="tex" type="text" placeholder='Realm name' name="realm"/>
+<input className="tex" type="text" placeholder='Realm name' name="realm" onChange={event => setRealm(event.target.value)}/>
 </label>
 
   <label>
-    <input className="tex" type="text" placeholder='Character name' name="char"  />
+    <input className="tex" type="text" placeholder='Character name' name="char" onChange={event => setName(event.target.value)} />
   </label>
 
   <div></div>
 
-  <button type="submit" className="subm" onClick={fetchwAffix}>Check Score</button>
-      
       </div>
       
-      </form>
+      <button className="subm" onClick={onClickHandler}>Check Score</button>
      <div>
-     <p>{wAffix}</p>
+     <h3 id='scoreText'> {url} </h3>
+     <div></div>
      </div>
 
     </div>
